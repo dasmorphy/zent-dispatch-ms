1cb4d23c82509ce0610856aea9fa66e3781d76e5
Daniel Males <danielmaleswilliams@hotmail.com>
2026-03-26 17:34:21 -0500

    dipatch repository

--- swagger_server/repository/dispatch_repository.py
+++ swagger_server/repository/dispatch_repository.py
@@ -1,3 +1,7 @@
+from loguru import logger
+
+from swagger_server.exception.custom_error_exception import CustomAPIException
+from swagger_server.models.db.dispatch import Dispatch
 from swagger_server.resources.databases.postgresql import PostgreSQLClient
 
 
@@ -6,3 +10,66 @@ class DispatchRepository:
     def __init__(self):
         self.db = PostgreSQLClient("POSTGRESQL")
         # self.redis_client = RedisClient()
+
+    
+    def post_dispatch(self, body, images, internal, external):
+        saved_files = []
+
+        with self.db.session_factory() as session:
+            try:
+                dispatch = Dispatch(
+                    vehicle_type_id=body.get('vehicle_type'),
+                    destiny_id=body.get('destiny'),
+                    driver=body.get('driver'),
+                    observations=body.get('observations'),
+                    truck_license=body.get('truck_license'),
+                    quantity=body.get('quantity'),
+                    weight=body.get('weight'),
+                    provider=body.get('provider'),
+                    truck_license=body.get('truck_license'),
+                    weight=body.get('name_driver'),
+                    created_by=body.get('user'),
+                    updated_by=body.get('user'),
+                )
+
+                self.saveImages(session)
+                session.commit()
+
+            except Exception as exception:
+                session.rollback()
+                logger.error('Error: {}', str(exception), internal=internal, external=external)
+                if isinstance(exception, CustomAPIException):
+                    raise exception
+                
+                raise CustomAPIException("Error al insertar en la base de datos", 500)
+
+            finally:
+                session.close()
+
+
+    def saveImages(self, session, data: RequestIdempotency, internal, external):
+        try:
+            
+            uuid_request_exist = session.execute(
+                select(
+                    exists().where(
+                        RequestIdempotency.uuid == data.uuid
+                    )
+                )
+            ).scalar()
+
+            if uuid_request_exist:
+                raise CustomAPIException(
+                    message="Duplicación de registro, el external transaction ya existe",
+                    status_code=409
+                )
+
+            session.add(data)
+            # session.commit()
+
+        except Exception as exception:
+            logger.error('Error: {}', str(exception), internal=internal, external=external)
+            if isinstance(exception, CustomAPIException):
+                raise exception
+            
+            raise CustomAPIException("Error al buscar en la base de datos", 500)
--- swagger_server/uses_cases/dispatch_use_case.py
+++ swagger_server/uses_cases/dispatch_use_case.py
@@ -2,6 +2,7 @@
 
 
 
+from swagger_server.exception.custom_error_exception import CustomAPIException
 from swagger_server.models.db.dispatch import Dispatch
 from swagger_server.repository.dispatch_repository import DispatchRepository
 
@@ -13,28 +14,9 @@ class DispatchUseCase:
 
 
     def post_dispatch(self, body, images, internal_process: tuple) -> None:
+
+        if len(images) > 10:
+            raise CustomAPIException("Máximo 10 imagenes", 500)
+        
         internal, external = internal_process
-
-        dispatch = Dispatch(
-            unity_id=body['id_unity'],
-            category_id=body['id_category'],
-            group_business_id=body['id_group_business'],
-            shipping_guide=body['shipping_guide'],
-            description=body['description'],
-            quantity=body['quantity'],
-            weight=body['weight'],
-            provider=body['provider'],
-            truck_license=body['truck_license'],
-            name_driver=body['name_driver'],
-            destiny_intern=body['destiny_intern'],
-            authorized_by=body['authorized_by'],
-            observations=body['observations'],
-            created_by=body['created_by'],
-            updated_by=body['created_by'],
-            name_user=body['name_user'],
-            workday=body['workday'],
-            lat=body.get('lat'),
-            long=body.get('long')
-        )
-
-        self.logbook_repository.post_dispatch(dispatch, images, internal, external)
+        self.logbook_repository.post_dispatch(body, images, internal, external)
